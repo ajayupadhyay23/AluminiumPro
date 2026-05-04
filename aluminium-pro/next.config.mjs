a74cc34f534fdb4df7428b8ddab5d522daf7c3c2
ajayupadhyay23 <upadhyayajay2235@gmail.com>
2026-05-04 11:39:22 +0530

    Build: Added image remote patterns for production

--- aluminium-pro/next.config.mjs
+++ aluminium-pro/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'api.qrserver.com' },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
